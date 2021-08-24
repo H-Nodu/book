68486dfc7129b700f17ffcde3a6e518a622f56e9
Noda <h-noda@t-answer.jp>
2021-08-25 01:01:01 +0900

    メニュー表示

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import Header from './components/header';
 
@@ -11,7 +12,23 @@ const onClickHandler = () => {
 function App() {
   return (
     <div>
+
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+
       <Header/>
+      <CircularProgress color="secondary" />
       <Button variant="contained" color="primary" onClick={() => onClickHandler()}>
         Hello World
       </Button>
--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,63 @@
 import React from 'react'
-import { AppBar, Toolbar, makeStyles, Typography } from '@material-ui/core'
-import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import clsx from 'clsx';
+import { AppBar, Toolbar, makeStyles, Typography, Drawer, MenuItem, IconButton } from '@material-ui/core'
+
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles(() => ({
-    typographyStyles: {
-      flex: 1
-    }
-  }));
+  typographyStyles: {
+    flex: 1
+  }
+}));
 
 function Header() {
-    
-    const classes = useStyles();
 
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography className={classes.typographyStyles}>
-                Book
-                </Typography>
-                <AccountCircleOutlinedIcon />
-            </Toolbar>
-        </AppBar>
-    )
+  // stateとstateを更新するための関数を宣言
+  const [open, setOpen] = React.useState(false);
+
+  // メニューを開く関数
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  // メニューを開く関数
+  const handleDrawerClose = () => {
+    console.log('Handle Toggle')
+    setOpen(false);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            TOP
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div>
+          <Drawer 
+          open={open}
+          onClick={handleDrawerClose}
+          >
+            <MenuItem>ログアウト</MenuItem>
+          </Drawer>
+      </div>      
+    </div>
+  )
 }
 
 export default Header
