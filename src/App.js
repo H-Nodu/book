d28e8f4eba4888ea753d5bfc8985a00f537e633b
Noda <h-noda@t-answer.jp>
2021-08-25 04:04:28 +0900

    フルスクリーンダイアログを追加してみた

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/organisms/header';
 import Footer from './components/organisms/footer';
 import PageRouter from './components/templates/router';
 
+import FullScreenDialog from './components/organisms/full_screen_dialog';
+
 function App() {
 
   // イベント
@@ -24,6 +26,7 @@ function App() {
       <Button variant="contained" color="primary" onClick={event.click}>
         Hello World
       </Button>
+      <FullScreenDialog />
       <Footer />
     </div>
   );
