6b9663b90902f31aeda5fbe9f5762aef4a98b97d
Noda <h-noda@t-answer.jp>
2021-08-24 11:39:28 +0900

    ボタンイベント実装

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Header from './components/header';
 
+//クリックハンドラー
+const onClickHandler = () => {
+  window.alert("clickされた");
+};
+
 function App() {
   return (
     <div>
       <Header/>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={() => onClickHandler()}>
         Hello World
       </Button>
     </div>
